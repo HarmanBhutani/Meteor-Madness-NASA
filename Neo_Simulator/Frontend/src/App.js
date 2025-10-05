e2c3dbb24a6ccc1f01a7b895339027fa13a98843
Gurnoorsingh wadhwa <gurnoorw09@gmail.com>
2025-10-05 12:18:03 +0530

    latest

---
+++ Neo_Simulator/Frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AsteroidVisualiser3D from "./AsteroidVisualiser3D";
+const AsteroidVisualiser = AsteroidVisualiser3D;
+
+
+function App() {
+  return (
+    <div>
+      <AsteroidVisualiser3D />
+    </div>
+  );
+}
+
+export default App;
